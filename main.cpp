56d226677fe1b981981b7e992e598c90e9ba1e74
Amit Das Gupta <amitdsgupta007@gmail.com>
2017-10-24 18:45:09 +0530

    given a height h give an algorithm for generating HB(0)

--- main.cpp
+++ main.cpp
@@ -16,7 +16,19 @@ public:
         this->left=NULL;
     }
 };
-
+/********************inserting into avl tree*////////////////////
+void insertIntoAvl(AvlBst* &root,int data)
+{
+    if(!root||root->data==data)
+    {
+        root=new AvlBst(data);
+    }
+    else
+        if(data<root->data)
+        insertIntoAvl(root->left,data);
+    else
+        insertIntoAvl(root->right,data);
+}
 /**************************inOrder traversal of avl tree*//////////////////////
 void inOrderTraversal(AvlBst* root)
 {
@@ -29,115 +41,48 @@ void inOrderTraversal(AvlBst* root)
         inOrderTraversal(root->right);
     }
 }
-/**********************preorder traversal*///////////////////////
-void preOrder(AvlBst* root)
-{
-    if(!root)
-        return;
-    cout<<root->data<< " ";
-    preOrder(root->left);
-    preOrder(root->right);
-}
 /**************************height of avl tree*///////////////////
 int heightOfAvlBst(AvlBst* root)
 {
     if(!root)
         return 0;
-    else return max(heightOfAvlBst(root->left),heightOfAvlBst(root->right))+1;
+    else
+        return max(heightOfAvlBst(root->left),heightOfAvlBst(root->right))+1;
 }
 /******************differnce of heights between left and right subtree*///////////////////
 int heightDifferenceAvl(AvlBst* root)
 {
-    if(!root->left)
-        return root->right->height;
-    if(!root->right)
-        return root->left->height;
+    return abs(heightOfAvlBst(root->left)-heightDifferenceAvl(root->right));
+
+}
+/*************************************given a height h give an algorithm for generating HB(0)*////////////////////////////
+AvlBst* giveTreeHB(int h,int &i)
+{
+    if(h==0)
+        return 0;
     else
-    return abs(root->left->height-root->right->height);
-}
-
-
-
-/*************************single rotation left*/////////////////////////////////////////
-AvlBst* singleRoltationLeft(AvlBst* root)
-{
-    AvlBst* left=root->left;
-    root->left=left->right;
-    left->right=root;
-    left->height=max(heightOfAvlBst(left->right),heightOfAvlBst(left->left))+1;
-    root->height=max(heightOfAvlBst(root->left),heightOfAvlBst(root->right))+1;
-    return left;
-}
-/****************************single rotation right*//////////////////////////////////
-AvlBst* singleRoltationRight(AvlBst* root)
-{
-    AvlBst* x=root->right;
-    root->right=x->left;
-    x->left=root;
-    x->height=max(heightOfAvlBst(x->right),heightOfAvlBst(x->left))+1;
-    root->height=max(heightOfAvlBst(root->left),heightOfAvlBst(root->right))+1;
-    return x;
-}
-/***********************************rotation in right subtree of left child*///////////////////////
-AvlBst* doubleRotationLeft(AvlBst* root)
-{
-    root->left=singleRoltationRight(root->left);
-    return singleRoltationLeft(root);
-}
-/****************************rotation in left subtree of right *////////////////////////////
-AvlBst* doubleRotationRight(AvlBst* root)
-{
-    root->right=singleRoltationRight(root->right);
-    return singleRoltationRight(root);
-}
-/********************inserting into avl tree*////////////////////
-void insertIntoAvl(AvlBst* &root,int data)
-{
-    if(!root||root->data==data)
     {
-        root=new AvlBst(data);
-
-     //   root->height=heightOfAvlBst(root);
+        AvlBst* head=new AvlBst(i);
+        i++;
+        head->left=giveTreeHB(h-1,i);
+        head->right=giveTreeHB(h-1,i);
     }
-    else
-        if(data<root->data)
-        {
-            //cout<<root->data;
-            insertIntoAvl(root->left,data);
-            root->left->height=heightOfAvlBst(root->left);
-             if(heightDifferenceAvl(root)==2)
-             {
-                 if(data<root->left->data)
-                    root=singleRoltationLeft(root);
-                 else
-                    root=doubleRotationLeft(root);
-             }
-        }
-    else
-        {
-            insertIntoAvl(root->right,data);
-            root->right->height=heightOfAvlBst(root->right);
-             if(heightDifferenceAvl(root)==2)
-             {
-                 if(data>root->right->data)
-                    root=singleRoltationRight(root);
-                 else
-                    root=doubleRotationRight(root);
-             }
-        }
 }
 int main()
 {
     AvlBst* root=NULL;
-    insertIntoAvl(root,4);
-    insertIntoAvl(root,2);
-    insertIntoAvl(root,7);
+    /*insertIntoAvl(root,10);
     insertIntoAvl(root,6);
-    insertIntoAvl(root,5);
-    insertIntoAvl(root,8);
-    preOrder(root);
-    cout<<"\n";
+    insertIntoAvl(root,16);
+    insertIntoAvl(root,4);
+    insertIntoAvl(root,9);
+    insertIntoAvl(root,13);
+    insertIntoAvl(root,7);
     inOrderTraversal(root);
-     //cout<<root->height;
+    */
+    int h,i=1;
+    cin>>h;
+    root=giveTreeHB(h,i);
+    inOrderTraversal(root);
     return 0;
 }
